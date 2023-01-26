5ade216e27cdd77d617aa073330a13ec4f63e717
0noder4 <kuklewskibartosz@gmail.com>
2023-01-26 17:39:56 +0100

    basic login logic

--- resources/js/features/login/components/LoginForm.js
+++ resources/js/features/login/components/LoginForm.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GuacamoleButton, GuacamoleInput } from '../../../mui'
+import login from '../services/api'
 
 export default function LoginForm() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
   return (
     <div className='login-form'>
         <div className='welcome'>
@@ -10,14 +13,14 @@ export default function LoginForm() {
         </div>
         <div className='login-form-input'>
             <label className='login-form-label'>Nazwa użytkownika</label>
-            <GuacamoleInput id="username" variant="outlined" size="small"/>
+            <GuacamoleInput id="username" variant="outlined" size="small" onChange={(e) => {setUsername(e.target.value)}}/>
         </div>
         <div className='login-form-input' id='password'>
             <label className='login-form-label'>Hasło</label>
-            <GuacamoleInput id="password" variant="outlined" type="password" size="small"/>
+            <GuacamoleInput id="password" variant="outlined" type="password" size="small" onChange={(e) => {setPassword(e.target.value)}}/>
         </div>
         <div className='login-form-submit'>
-            <GuacamoleButton variant='contained'>Zaloguj</GuacamoleButton>
+            <GuacamoleButton variant='contained' onClick={() => {login(username, password)}}>Zaloguj</GuacamoleButton>
         </div>
     </div>
   )
---
+++ resources/js/features/login/services/api.js
@@ -0,0 +1,52 @@
+const MAIN_URL = 'http://localhost:8080/api';
+
+/**
+ * @param {string} endpoint 
+ * @param {RequestInit} requestOptions
+ **/
+
+export async function api(endpoint, requestOptions) {
+    const requestUrl = `${MAIN_URL}${endpoint}`;
+
+    if(!requestOptions.headers) {
+        requestOptions.headers = {}
+    }
+    if(requestOptions.body) {
+        requestOptions.headers["Accept"] = "application/json"
+        requestOptions.headers["Content-Type"] = "application/json"
+    }
+    if(localStorage.getItem("token")) {
+        requestOptions.headers["Authorization"] = localStorage.getItem("token");
+    }
+
+    //Authorization logic
+
+    const response = await fetch(requestUrl, requestOptions)
+    const result =  await response.json()
+        if(response.ok) {
+            return result
+        }
+        else {
+            return {
+                "code": response.status,
+                "message": result.message
+            }
+        }
+
+}
+
+export default async function login(username, password) {
+    const endpoint = "/login"
+    const body = JSON.stringify({
+        "username": username,
+        "password": password
+    });
+
+    const result = await api(endpoint, {body: body, method:'POST'});
+
+    if(result.token) {
+        localStorage.setItem("token", result.token)
+        console.log("token set")
+    }
+    console.log(result)
+}
