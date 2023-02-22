24f35807bd9e377388d7e0e74ef6b1e782111ac5
Karol Krzykowski <karol_krzykowski@wp.pl>
2023-02-22 21:09:06 +0100

    fix: strings in migration

--- database/migrations/2023_01_18_163546_create_class_rooms_table.php
+++ database/migrations/2023_01_18_163546_create_class_rooms_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('class_rooms', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
-            $table->text("description");
+            $table->string('name');
+            $table->text('description');
             $table->timestamps();
         });
     }
