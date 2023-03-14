38e92c7a1a87c683f6e4a9722f885aef75fe6f39
Karol Krzykowski <karol_krzykowski@wp.pl>
2023-03-14 21:46:10 +0100

    feat: added more objects in get lectures responses

--- app/Action/Lecture/LectureGetAllStudentAction.php
+++ app/Action/Lecture/LectureGetAllStudentAction.php
@@ -2,7 +2,9 @@
 
 namespace App\Action\Lecture;
 
+use App\Models\ClassRoom;
 use App\Models\Lecture;
+use App\Models\StudentClass;
 use App\Models\StudentClasses;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
@@ -16,6 +18,17 @@ class LectureGetAllStudentAction
         foreach ($studentClasses as $student) {
            $where[] = ['class_id', '=', $student->student_class];
         }
-        return Lecture::where($where)->get();
+        $lectures = Lecture::where($where)->get();
+
+        foreach ($lectures as &$lecture) {
+            $lecture->instructor = User::find($lecture->instructor_id);
+            $lecture->class_room = ClassRoom::find($lecture->class_room_id);
+            $lecture->class = StudentClass::find($lecture->class_id);
+             
+            unset($lecture->instructor_id);
+            unset($lecture->class_room_id);
+            unset($lecture->class_id);
+        }
+        return $lectures;
     }
 }
