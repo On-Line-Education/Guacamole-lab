3f515b2d49a2ad9abc477f4b1a57f91899204f3d
0noder4 <kuklewskibartosz@gmail.com>
2023-03-16 21:29:48 +0100

    feat: lesson calendar and filter

--- resources/js/features/lessons/index.js
+++ resources/js/features/lessons/index.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Logo from "../../components/Logo/Logo";
-import "./assets/style.scss";
+import "./style.scss";
 import LessonReserve from "./components/LessonReserve/LessonReserve";
 import LessonReservedList from "./components/LessonReservedList/LessonReservedList";
 import useGetAllReserved from "./hooks/useGetAllReserved";
 import ReservationDetails from "./components/ReservationDetails/ReservationDetails";
+import LessonCalendar from "./components/LessonCalendar/LessonCalendar";
+import dayjs from "dayjs";
 
 export default function LessonsView() {
     // View states
@@ -13,9 +15,10 @@ export default function LessonsView() {
     const [reservationDetailsPanelState, setReservationDetailsPanelState] =
         useState(false);
 
-    // Selected table row
+    // Selected table row and calendar date
 
     const [selectedReservation, setSelectedReservation] = useState("");
+    const [date, setDate] = useState(dayjs());
 
     // Queries
 
@@ -31,6 +34,12 @@ export default function LessonsView() {
             <Logo />
             <Sidebar active={"lessons"} />
             <div className="lessons-container">
+                <LessonCalendar
+                    date={date}
+                    setDate={setDate}
+                    reservedList={reservedList}
+                    loading={reservedListLoading}
+                />
                 <LessonReservedList
                     reservedList={reservedList}
                     loading={reservedListLoading}
@@ -39,6 +48,7 @@ export default function LessonsView() {
                     setReservationDetailsPanelState={
                         setReservationDetailsPanelState
                     }
+                    date={date.format()}
                 />
                 <LessonReserve refetch={reservedListRefetch} />
             </div>
