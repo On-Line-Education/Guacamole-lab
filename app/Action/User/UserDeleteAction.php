9f6e3c6520f4cf2999eee988d85418259ea2e0d4
Karol Krzykowski <karol_krzykowski@wp.pl>
2023-03-23 00:13:02 +0100

    fix: user delete invalid column

--- app/Action/User/UserDeleteAction.php
+++ app/Action/User/UserDeleteAction.php
@@ -20,7 +20,7 @@ class UserDeleteAction
     public function __invoke(GuacamoleAuthLoginData $guacamoleAuthLoginData, User $user): ?bool
     {
         $this->guacamole->getUser()->delete($guacamoleAuthLoginData, $user->username);
-        Computer::where('user_id', $user->id)->update(['user_id', null]);
+        Computer::where('user_id', $user->id)->update(['user_id' => null]);
         GuacUserData::where('user_id', $user->id)->delete();
         StudentClasses::where('student', $user->id)->delete();
         $user->delete();
